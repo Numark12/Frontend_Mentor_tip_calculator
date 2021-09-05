e86e62ec59e0cd8342a57a6d6663659cbc8fd4ad
Your NameNumark12 <fernandoerreca@gmail.com>
2021-09-05 16:18:48 -0300

    versión final.

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ var custom = document.getElementById("custom");
 var final = []
 var cuenta = []
 
-document.getElementById("custom").disabled = true;
-document.getElementById("pers").disabled = true;
-function habilite_cstm(){
-    document.getElementById("custom").disabled = false;
-}
-function habilite_ppl(){
-document.getElementById("pers").disabled = false;
-}
+// DEJO ESTO ACÁ PARA VER CÓMO SE DESHABILITA UN INPUT (Y SE HABILITA);
+// document.getElementById("custom").disabled = true;
+// document.getElementById("pers").disabled = true;
+// function habilite_cstm(){
+//     document.getElementById("custom").disabled = false;
+// }
+// function habilite_ppl(){
+// document.getElementById("pers").disabled = false;
+// }
 
 // con esta función tomo el id del botón al pasar el mouse por encima del botón
 
@@ -58,13 +59,20 @@ function calculo(){
     //  El eventListener se pone dentro de la función, porque sino se ejecuta al cargar la página 
     //  con valores vacíos y da como resultado NaN.  
         let tip =  parseFloat(((cuenta[0]*final[0])/cuenta[1]).toFixed(2)) 
-        let factura = parseFloat((cuenta[0]/cuenta[1]).toFixed(2)) + tip;
+        let factura = (parseFloat((cuenta[0]/cuenta[1]).toFixed(2)) + tip).toFixed(2);
+
+        if(document.getElementById("bill").value==="" || document.getElementById("pers").value===""){
+            propina.innerHTML = "0.00";
+            boleta.innerHTML=  "0.00";  
+        } else {
+
         propina.innerHTML = tip;
         boleta.innerHTML=  factura;
-
+}
         document.getElementById("custom").value = ""
         document.getElementById("custom").placeholder = "Custom"
 }
+
 
 // Función para calcular valor personalizado
 function calcustom(){
@@ -76,23 +84,35 @@ function calcustom(){
     let tip =  parseFloat(((cuenta[0]*final[0])/cuenta[1]).toFixed(2)); 
     let factura = (parseFloat((cuenta[0]/cuenta[1]).toFixed(2)) + tip).toFixed(2);
 
-  
+ 
+    if(document.getElementById("bill").value==="" || document.getElementById("pers").value===""){
+        propina.innerHTML = "0.00";
+        boleta.innerHTML=  "0.00";  
+    } else {
+
     propina.innerHTML = tip;
-    boleta.innerHTML=  factura;  
+    boleta.innerHTML=  factura;
+}
 }
 
 
 // Función del display "can't be zero"
 function showDisclaim(){
-    document.getElementById("bill").disabled=true;
+    // document.getElementById("bill").disabled=true;
     document.getElementById("disclaim").style.display="flex"
 }
 
 function hide(){
-    const disclaim = document.getElementById("disclaim")
-    const pplNotZero = document.getElementById("pers").value;
-    if(pplNotZero!=""){
+   let disclaim = document.getElementById("disclaim")
+   let pplNotZero = document.getElementById("pers").value;
+   let billNotZero = document.getElementById("bill").value; 
+    if(pplNotZero!="" && pplNotZero!=0){
         disclaim.style.display="none";
+        document.getElementById("pers").style.border = "none";
+    }
+
+    if(billNotZero!="" && billNotZero!=0){
+        document.getElementById("bill").style.border = "none";
     }
 }
 
@@ -107,6 +127,10 @@ function hide(){
 
 
 // Función del reset
+
+const pplReset = document.getElementById("pers");
+const billReset = document.getElementById("bill");  
+
 const reset = document.getElementById("reset");
 reset.addEventListener("click", function(){
     document.getElementById("custom").value = ""
@@ -116,6 +140,16 @@ reset.addEventListener("click", function(){
     document.getElementById("pers").value = ""
     document.getElementById("pers").placeholder = "0"
     document.getElementById("bill").disabled=false;
+    boleta.innerHTML = "0.00";
+    propina.innerHTML = "0.00";
+    disclaim.style.display="none";
+    document.getElementById("pers").style.border = "none";
+    pplReset.addEventListener("focus", function(){
+        pplReset.style.border = "1px solid rgb(236, 153, 153)"
+    })
 
+    billReset.addEventListener("focus", function(){
+        billReset.style.border = "1px solid rgb(184, 230, 184)"
+    })
 }); 
 
